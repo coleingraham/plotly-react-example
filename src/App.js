f7effcf8f9dfb8c8f414e4474c78bbd5acee9f00
Cole Ingraham <coleingraham@Coles-MacBook-Pro.local>
2024-04-03 13:50:36 -0400

    got it working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Plot from 'react-plotly.js';
+import { Amplify, API } from 'aws-amplify';
 
-class App extends React.Component {
-  render() {
+const amplifyConfig = {
+    API: {
+        endpoints: [
+            {
+                name: 'exampleApi',
+                endpoint: 'https://qojy5wzkvc.execute-api.us-east-2.amazonaws.com/prod'
+            }
+        ]
+    }
+}
+
+Amplify.configure(amplifyConfig);
+
+async function callApi() {
+    const data = await API.get(
+        'exampleApi',
+        '/nvda')
+
+    return data
+}
+
+const App = () => {
+    const [data, setData] = useState({x: [], y: []})
+    const [revision, setRevision] = useState(0);
+
+    useEffect(() => {
+        callApi().then((payload) => {
+            let body = JSON.parse(payload.body)
+            setData(body)
+            setRevision(r => r+1)
+        })
+    }, [])
+
+    const layout = {
+        width: 1500,
+        height: 800,
+        title: 'NVidia Stock',
+        xaxis: {
+            type: "date"
+        },
+        dragmode: "pan",
+    };
+
+    const config = {
+        displayModeBar: false,
+        responsive: true
+    };
+
     return (
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      />
+        <Plot
+            data={[
+                {
+                    x: data.x,
+                    y: data.y,
+                    type: 'scatter',
+                    mode: 'lines',
+                    marker: {color: 'red'},
+                }
+            ]}
+            layout={layout}
+            config={config}
+            revision = {revision}
+        />
     );
-  }
 }
 
 export default App
